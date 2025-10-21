08b57a491f23086fa1356e73b36a22d25e61a2d2
9GotStory <thanakorn.kumchuen@hotmail.com>
2025-10-21 16:29:12 +0700

    chore: initial project setup

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
